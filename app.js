405e217069518a0518659de1fa492b93629a8212
AlexPystsov <4u4uk1337@gmail.com>
2022-09-17 23:03:46 +0300

    movi search

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const get_api_res_btn = document.getElementById('get_api_res_btn');
+const searchTitleField = document.getElementById('searchTitle');
 const api_result1 = document.getElementById('api_result1');
-const api_result2 = document.getElementById('api_result2');
+
 get_api_res_btn.addEventListener('click', getAPI);
 
-
-let endPoint = 'http://api.open-notify.org/iss-now.json';
-
-    function getAPI(){
-        fetch(endPoint, {
-		method: 'GET'
-	})
-    .then(res => res.json())
-    .then(data=> {
-        api_result1.innerHTML=data.iss_position.longitude;
-        api_result2.innerHTML=data.iss_position.latitude;
-        
-    });
+function getAPI(){
+	fetch(
+		'http://www.omdbapi.com/?apikey=63782376&t=' + searchTitleField.value,
+		{ method: 'GET' }
+	)
+	.then(res => res.json())
+	.then(data => {
+		console.log(data);
+		
+		if (data.Response === 'False') {
+			api_result1.style.display = 'none';
+			
+			return;
+		}
+		
+		api_result1.style.display = 'block';
+		api_result1.innerHTML = 'Title: ' + data.Title + '<br>Year: ' + data.Year + '<br>Director: ' + data.Director;
+	});
 }
